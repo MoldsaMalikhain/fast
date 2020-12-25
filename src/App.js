6ec36e3ba94eb44822f291b10a204b72e5d3d995
Moldas Malikhain <kerch999@gmail.com>
2020-12-25 19:35:20 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import list from './list'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      inCart:[]
+    }
+    this.setSelected = this.setSelected.bind(this);
+    this.checkItems = this.checkItems.bind(this);
+  }
+  checkItems(){
+    let counter = 0;
+    this.state.inCart.forEach(element => {
+      if(element.color === 'blue')counter++;
+      else counter --;
+
+    });
+    
+    if(counter === 3) alert("Congrats!!!")
+    else alert("err!")
+  }
+  setSelected(item){
+    const cartItems = this.state.inCart.slice();
+    let isIn = false;
+    cartItems.forEach(element => {
+      console.log('item ID: ' + item.id + 'element ID: ' + element.id)
+
+      if(item.id === element.id){
+        isIn = true;
+      }
+    });
+    if(isIn === false) {
+      cartItems.push({...item})
+      this.setState({inCart: cartItems})
+    }
+    console.log(cartItems)
+    console.log(this.state.inCart)
+  }
+  render(){
+    return(
+      <div className="App">
+        <div className='handler'>
+          {list.map((item,key)=>{
+            return(
+              <div key={key++} 
+                  className='checkbox'
+                  value={this.state.isSelected}
+                  onClick={()=> this.setSelected(item)}
+                  style={{backgroundColor:item.color}}>
+              </div>
+            );
+          })}
+        </div>
+        <button className='submit' onClick={this.checkItems}>
+          submit
+        </button>
+        </div>
+      )
+  }
 }
 
 export default App;
---
+++ src/list.js
@@ -0,0 +1,30 @@
+const list =
+        [
+            {
+            id: 0,
+            color: "blue"
+            },
+            {
+            id: 1,
+            color: "blue"
+            },
+            {
+            id: 2,
+            color: "blue"
+            },
+            {
+            id: 3,
+            color: "green"
+            },
+            {
+            id: 4,
+            color: "red"
+            },
+            {
+            id: 5,
+            color: "black"
+            }
+        ]
+
+
+export default list;    
